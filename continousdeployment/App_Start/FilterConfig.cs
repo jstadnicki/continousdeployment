2e4a0c9331ea41306f0bc437de92e243b8b0fbcd
Jaroslaw Stadnicki (PGS Software) <jstadnicki@pgs-soft.com>
2017-06-03 00:00:22 +0200

    001

---
+++ continousdeployment/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace continousdeployment
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
